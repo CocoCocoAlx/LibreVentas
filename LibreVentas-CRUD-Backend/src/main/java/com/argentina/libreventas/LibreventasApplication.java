791f55bd85008a88919d6b3558efef16e9ecb73d
CocoCocoAlx <vania182004@gmail.com>
2022-06-17 12:54:27 -0400

    Initial commit

---
+++ LibreVentas-CRUD-Backend/src/main/java/com/argentina/libreventas/LibreventasApplication.java
@@ -0,0 +1,13 @@
+package com.argentina.libreventas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibreventasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibreventasApplication.class, args);
+	}
+
+}
---
+++ LibreVentas-CRUD-Backend/src/test/java/com/argentina/libreventas/LibreventasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.argentina.libreventas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibreventasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
